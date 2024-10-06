ed8b249aff43d6a6dc6ba1637f1293c55706057d
fangyuwu@weflywifi.com <fangyuwu@weflywifi.com>
2024-10-06 19:37:15 +0800

    bugfix

--- build.sh
+++ build.sh
@@ -100,7 +100,7 @@ termux_step_post_make_install() {
 	# many parts are taken directly from Arch PKGBUILD
 	# https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/nginx
 
-	rm "$TERMUX_PREFIX"/etc/nginx/*.default
+	echo "every thing done"
 }
 
 termux_step_post_massage() {
